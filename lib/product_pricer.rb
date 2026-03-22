b344a88861fcb22145c996dc90f62aacfe6b7088
Nert1166 <nikgorbunov2@gmail.com>
2026-03-22 19:02:45 +0300

    Added public interface for pricer

--- lib/product_pricer.rb
+++ lib/product_pricer.rb
@@ -1,8 +1,35 @@
 # frozen_string_literal: true
 
 require_relative "product_pricer/version"
+require_relative "product_pricer/errors"
+require_relative "product_pricer/calculation_context"
+require_relative "product_pricer/rules/base"
+require_relative "product_pricer/rules/delivery_rule"
+require_relative "product_pricer/rules/weight_rule"
+require_relative "product_pricer/rules/tax_rule"
+require_relative "product_pricer/rules/promo_rule"
+require_relative "product_pricer/rules/loyalty_rule"
+require_relative "product_pricer/rules/round_price_rule"
+require_relative "product_pricer/pricer"
 
 module ProductPricer
   class Error < StandardError; end
-  # Your code goes here...
+
+  def self.calculate(product:, region:, promo_code: nil, quantity: 1, user_tier: nil, config_dir: nil)
+    config_dir ||= File.join(File.dirname(__FILE__), "..", "config")
+
+    pricer = Pricer.new(
+      delivery_config: File.join(config_dir, "delivery.json"),
+      tax_config: File.join(config_dir, "taxes.json"),
+      discount_config: File.join(config_dir, "sales.json")
+    )
+
+    pricer.calculate(
+      product: product,
+      region: region,
+      promo_code: promo_code,
+      quantity: quantity,
+      user_tier: user_tier
+    )
+  end
 end
