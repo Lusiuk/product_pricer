aaab534a1614354830841449cbcb69b9a0967bcc
Nert1166 <nikgorbunov2@gmail.com>
2026-03-22 18:29:55 +0300

    Added rule handling taxes calculations

---
+++ lib/product_pricer/rules/tax_rule.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require "bigdecimal"
+
+module ProductPricer
+  module Rules
+    class TaxRule < Base
+      def priority
+        100
+      end
+
+      def apply(context)
+        return context unless @config
+
+        category = context.product.category
+        region = context.region
+
+        tax_rate = @config.dig("categories", category, region)
+        return context unless tax_rate
+
+        tax_rate = BigDecimal(tax_rate.to_s)
+
+        # Decide what to tax: base price, delivery, or both
+        tax_shipping = @config.dig("rules", "tax_shipping") || false
+        taxable_amount = context.base_price
+        taxable_amount += context.delivery_cost if tax_shipping
+
+        tax_amount = taxable_amount * tax_rate
+        context.tax_amount = tax_amount
+        context.track_rule("tax", { tax_rate: tax_rate, taxable_amount: taxable_amount })
+
+        context
+      end
+    end
+  end
+end
