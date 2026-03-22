ba70065755e7554d48bdf5c1c2818afb665982c0
YariloDia <Xan-Yar0slav@yandex.ru>
2026-03-22 18:23:38 +0300

    Added a rule for final price calculation

---
+++ lib/product_pricer/rules/round_price_rule.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require "bigdecimal"
+
+module ProductPricer
+  module Rules
+    class RoundPriceRule < Base
+      def priority
+        999
+      end
+
+      def apply(context)
+        # Calculate final price
+        final = context.base_price
+        final += context.delivery_cost
+        final += context.weight_surcharge
+        final += context.tax_amount
+        final -= context.discount_amount
+
+        # Round to 2 decimal places
+        context.final_price = final.round(2)
+        context.track_rule("round", { final_price: context.final_price })
+
+        context
+      end
+    end
+  end
+end
