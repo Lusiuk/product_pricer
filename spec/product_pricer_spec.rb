91a63fb881a80d0bad9ba70155ef068aa042bbde
Nert1166 <nikgorbunov2@gmail.com>
2026-03-22 19:52:33 +0300

    Added tests for product pricer

--- spec/product_pricer_spec.rb
+++ spec/product_pricer_spec.rb
@@ -5,7 +5,17 @@ RSpec.describe ProductPricer do
     expect(ProductPricer::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe ".calculate" do
+    it "calculates simple price without configs" do
+      product = OpenStruct.new(price: 100, category: "electronics", weight: 1)
+
+      result = ProductPricer.calculate(
+        product: product,
+        region: "US"
+      )
+
+      expect(result).to be_a(ProductPricer::CalculationContext)
+      expect(result.base_price).to eq(BigDecimal("100"))
+    end
   end
 end
